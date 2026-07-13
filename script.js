0fc40863ee366844d82992b1d0811b4148c089f2
sahipushpak <4waygaming12@gmail.com>
2026-07-13 15:46:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-// ==========================
-// Mobile Navigation
-// ==========================
+@media (max-width: 991px) {
 
-const menuBtn = document.querySelector(".menu-btn");
-const navbar = document.querySelector(".navbar");
+    .menu-btn{
+        display:block;
+    }
 
-menuBtn.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
+    .navbar{
+        position:absolute;
+        top:80px;
+        left:0;
+        width:100%;
+        background:#ffffff;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        gap:25px;
+        padding:30px 0;
+        box-shadow:0 10px 20px rgba(0,0,0,.1);
 
-// ==========================
-// Smooth Scroll Close Menu
-// ==========================
+        transform:translateY(-150%);
+        transition:.4s ease;
+    }
 
-document.querySelectorAll(".navbar a").forEach(link => {
+    .navbar.active{
+        transform:translateY(0);
+    }
 
-    link.addEventListener("click", () => {
+    .hero{
+        grid-template-columns:1fr;
+        text-align:center;
+    }
 
-        navbar.classList.remove("active");
+    .hero-buttons{
+        justify-content:center;
+    }
 
-    });
+    .hero p{
+        margin:0 auto 35px;
+    }
 
-});
+}
