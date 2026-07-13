42a5de6f1ebed92d5e98f493498bbb8ad9547637
sahipushpak <4waygaming12@gmail.com>
2026-07-13 15:41:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// ==========================
+// Mobile Navigation
+// ==========================
 
+const menuBtn = document.querySelector(".menu-btn");
+const navbar = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+// ==========================
+// Smooth Scroll Close Menu
+// ==========================
+
+document.querySelectorAll(".navbar a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navbar.classList.remove("active");
+
+    });
+
+});
